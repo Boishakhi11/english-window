1222f4d6f946e116c48be682cc5ae50074e4d269
Boishakhi G <bgmukta11@gmail.com>
2025-12-15 01:32:13 +0100

    search added

--- index.js
+++ index.js
@@ -112,3 +112,22 @@ const displayLessons = (lessons) => {
 };
 
 loadLesson();
+
+document.getElementById("btn-search").addEventListener("click", ()=> {
+    const input = document.getElementById("input-search");
+    const searchValue = input.value.trim().toLowerCase();
+    console.log(searchValue);
+
+    fetch("https://openapi.programming-hero.com/api/words/all")
+    .then(res => res.json())
+    .then(data=> {
+        const allWords = data.data;
+        console.log(allWords);
+        const filterWords = allWords.filter(word=> word.word.toLowerCase().includes(searchValue)
+    );
+        displayLevelWord(filterWords);
+    });
+
+    
+})
+
